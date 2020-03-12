67d0bf90fd60e842723429b699b2529fbfc0082f
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-12 17:28:07 +0530

    updates

--- index.js
+++ index.js
@@ -3,5 +3,24 @@ var keycode = require('keycode')
 var KeyStream = require('./key_stream')
 
 module.exports = function(options){
-    
+    options = options || {}
+
+    var currentOffset = options.offset || 0
+    var currentVelocity = options.velocity || 127
+    var chan = options.channel || 0
+  
+    var modes = {
+      'piano': {
+        notes: "AWSEDFTGYHUJKOLP;['".toLowerCase().split(''),
+        offsetBy: 12,
+        offset: ['z', 'x'],
+        velocity: ['c', 'v']
+      },
+      'grid': {
+        notes: 'QWERTYUIASDFGHJKZXCVBNM,'.toLowerCase().split(''),
+        offsetBy: 8,
+        offset: ['-', '='],
+        velocity: ['[', ']']
+      }
+    }
 }
